a91e91b1bf607ed7dacb62532d36fd05d5e5abdb
Alessandro Ferrari <fullmetalsandro@gmail.com>
2018-10-18 14:43:48 +0200

    attempt on fixing swipe

--- public/swipe.js
+++ public/swipe.js
@@ -67,7 +67,7 @@ class Swipe {
 
     run() {
         this.element.addEventListener('touchmove', function(evt) {
-            this.handleTouchMove(evt).bind(this);
+            this.handleTouchMove(evt);
         }.bind(this), false);
     }
 }
